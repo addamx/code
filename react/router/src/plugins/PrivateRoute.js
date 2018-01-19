8ce90b8774722462cee9afa0544a64ab9462e965
addams <addams.x@infinitytechnologies.com.cn>
2018-01-19 17:40:27 +0800

    update

---
+++ react/router/src/plugins/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom'
+import fakeAuth from '../service/fakeAuth'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+export default PrivateRoute
