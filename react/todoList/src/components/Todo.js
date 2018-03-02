fa39b34e38449ddb28325f5d9cc5b822f3ba6748
addams <addams.x@infinitytechnologies.com.cn>
2018-03-02 18:50:16 +0800

    v

--- react/todoList/src/components/Todo.js
+++ react/todoList/src/components/Todo.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Todo = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    style={{
-      textDecoration: completed ? 'line-through' : 'none'
-    }}
-  >
-    {text}
-  </li>
-)
+const Todo = ({ onClick, completed, text }) => {
+  console.log('render');
+  return (
+    <li
+      onClick={onClick}
+      style={{
+        textDecoration: completed ? 'line-through' : 'none'
+      }}
+    >
+      {text}
+    </li>
+  )
+}
 
 export default Todo;
