a9416136b8da2ee7d95b3cb5aea0309e76bcb56a
xuhr <xuhr@mingyuanyun.com>
2020-06-29 01:11:56 +0800

    v

--- vue/gcxt-new/src/components/business/edit/service.js
+++ vue/gcxt-new/src/components/business/edit/service.js
@@ -1,5 +1,12 @@
-
+import { init } from "grunt"
 
 export default {
-
+  init(componentList, componentConfig, bizConfig) {
+  },
+  initStore(componentList) {
+    // ...
+    return {
+      [componentName]: {state: 1}
+    };
+  },
 }
