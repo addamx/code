651cc5ca0a1713063c66f55f074b6375ee747da8
xuhr <xuhr@mingyuanyun.com>
2020-07-05 23:58:33 +0800

    v

---
+++ gcxt-new/src/utils/ioc/index.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+ // http://www.bingshangroup.com/blog/2019/12/09/zhou/IoC/
+ // http://www.bingshangroup.com/blog/2019/12/09/zhou/DI/
+ // https://angular.cn/guide/dependency-injection-providers
+ // https://github.com/angular/di.js
+ // https://juejin.im/post/590d5f502f301e006c145f01
+ // https://zhuanlan.zhihu.com/p/72323250
+ // https://zhuanlan.zhihu.com/p/53832991
+export default class Ioc {
+  annotate() {
+    
+  }
+}
